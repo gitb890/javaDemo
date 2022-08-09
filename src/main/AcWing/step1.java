130b6e8689f460a9563d54c4192df418bae1f208
gitb890 <2832816260@qq.com>
2022-08-10 01:36:24 +0800

    语法练习题

---
+++ src/main/AcWing/step1.java
@@ -0,0 +1,14 @@
+package AcWing;
+
+import java.util.Scanner;
+
+/**A+B*/
+public class step1 {
+    public static void main(String[] args) {
+        int x,y;
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        y = in.nextInt();
+        System.out.println(x+y);
+    }
+}
---
+++ src/main/AcWing/step2.java
@@ -0,0 +1,17 @@
+package AcWing;
+
+import java.util.Scanner;
+
+/**乘积*/
+public class step2 {
+    public static void main(String[] args) {
+        int A,B,C,D;
+        Scanner in = new Scanner(System.in);
+        A = in.nextInt();
+        B = in.nextInt();
+        C = in.nextInt();
+        D = in.nextInt();
+        int DIFERENCA = (A*B-C*D);
+        System.out.println(DIFERENCA);
+    }
+}
